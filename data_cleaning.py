daa30a220dd3e229c2b6c9bd0ebe42b31e3ef0cf
Saverio Tufano <saverio.tufano@gmail.com>
2023-05-15 22:09:45 +0100

    Docstring mod to data_cleaning, upload_to_db created in the DatabaseConnector class

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,3 +1,13 @@
 
 class DataCleaning:
-    pass
+    """
+    Defines methods to clean data from all datasources
+    """
+    def clean_user_data():
+        
+        """
+        Performs the cleaning of the user data.
+        You will need clean the user data, look out for NULL values, errors with dates, incorrectly
+        typed values and rows filled with the wrong information.
+        """
+        pass
