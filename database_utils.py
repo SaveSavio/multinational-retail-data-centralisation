164e4f61ad00f2ca9934356792cb6606f64d4e4c
Saverio Tufano <saverio.tufano@gmail.com>
2023-05-15 17:13:30 +0100

    Initializes the three project classes

---
+++ database_utils.py
@@ -0,0 +1,37 @@
+class DatabaseConnector:
+    """
+    Defines the tools to connect and upload data into the database
+    Ref: Project Milestone2, Task2
+    Attributes:
+        ...
+        ...
+    """
+    
+    # def __init__(self)
+
+    def read_db_creds(creds_yaml):
+        """
+        Reads credentials from the yaml file and returns a dictionary of credentials
+        """
+        import yaml
+
+        with open(creds_yaml, 'r') as yaml_file:
+            creds_dict = yaml.safe_load(yaml_file)
+            print(creds_dict)
+            return(creds_dict)
+        
+    def init_db_engine(creds_yaml):
+        """
+        Reads the credentials from the return of read_db_creds and initialises and returns an sqlalchemy database engine.
+        """
+        credentials = read_db_creds(creds_yaml)
+
+        from sqlalchemy import create_engine
+        DATABASE_TYPE = 'postgresql'
+        DBAPI = 'psycopg2'
+        HOST = credentials['RDS_HOST']
+        USER = credentials['RDS_USER']
+        PASSWORD = credentials['RDS_PASSWORD']
+        DATABASE = credentials['RDS_DATABASE']
+        PORT = credentials['RDS_PORT']
+        engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}") 
---
+++ tester_file.py
@@ -0,0 +1,10 @@
+from database_utils import DatabaseConnector as dbc
+file_name = 'db_creds.yaml'
+
+# read credential and transform them from yaml to dict
+cred_dict = dbc.read_db_creds(file_name)
+print(cred_dict)
+print(type(cred_dict))
+
+# initialize engine
+dbc.init_db_engine(file_name)
