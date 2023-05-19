325f9ca27a1dda436fbecef7bb65bc7b99f68b01
Saverio Tufano <saverio.tufano@gmail.com>
2023-05-19 21:40:35 +0100

    improvement made to the clean_user_data method: search for incorrect dates and remove the corresponding rows

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,21 +1,50 @@
 
 class DataCleaning:
     """
-    Defines methods to clean data from all datasources
+    Defines methods to clean data the user data from various datasources.
     """
     def clean_user_data(df):
         
         """
-        Performs the cleaning of the user data.
-        You will need clean the user data, look out for NULL values, errors with dates, incorrectly
+        Performs the cleaning of the user data for NULL values, errors with dates, incorrectly
         typed values and rows filled with the wrong information.
+            Parameters:
+                A pandas dataframe
+            Returns:
+                The same dataframe, but cleaned.
+
         """
         import pandas as pd
+        import datetime
+
         # drop the index columns, it is redundant
         df = df.drop(['index'], axis = 1)
+        
+        # drop the rows containing NaNs
+        df = df[df.notna().any(axis=1)]
+
+        # transform the DOE from string to a datetime object. The errors option will
+        # set a NaT for every date that cannot be interpreted 
+        df['date_of_birth'] = pd.to_datetime(df['date_of_birth'], infer_datetime_format=True, errors = 'coerce')
+        # this statement allows to get rid of the rows with incorrect dates
+        df = df[df['date_of_birth'].notna()]
+
+        # let's do the same for join_date
+        df['join_date'] = pd.to_datetime(df['join_date'], infer_datetime_format=True, errors = 'coerce')
+        # this statement allows to get rid of the rows with incorrect dates
+        df = df[df['join_date'].notna()]
+        
         # sort out some wrong country code entries for UK
         df.loc[(df['country'] == "United Kingdom") & (df['country_code'] != "GB"), 'country_code'] = 'GB'
         # remove some wrong entries
         df = df[df['country'].isin(['United Kingdom', 'Germany', 'United States'])]
         # returns the cleaned dataframe if required
         return df
+    
+    def clean_card_data(df):
+        
+        """
+        Performs the cleaning of the card data.
+        Removes any erroneous values, NULL values or errors with formatting.
+        """
+        pass
