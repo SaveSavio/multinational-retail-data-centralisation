f014ec2abe69427ba8c51027c944cc7c62bcedb3
Saverio Tufano <saverio.tufano@gmail.com>
2023-06-02 15:44:02 +0100

    Milestone_4 file created. First two tasks completed

--- milestone_2.py
+++ milestone_2.py
@@ -1,74 +1,93 @@
-# import the DatabaseConnector class
-# it serves for connecting and uploading data from various sources
+"""
+This file contains the commands for the Milestone 2 of the Multinational Retail Data Centralisation project (AiCore)
+I runs the main commands that
+    1) Download from various sources
+    2) Clean the data
+    3) Upload the data to a Postgresql database named sales_data
+
+It uses three classes coded in the files
+    data_cleaning
+    data_extraction
+    database_utils
+"""
+
+# import the DatabaseConnector class for connecting and uploading data from various sources
 from database_utils import DatabaseConnector as dbc
 
-# this is the file with the credentials to connect the RDS AWS server
+# the credentials to connect the RDS AWS server are stored in this yaml file
 file_name = 'db_creds.yaml'
 
-# reads credential and transform them from yaml to dict
+# read credential and transform them from yaml to dict
 cred_dict = dbc.read_db_creds(file_name)
-print(cred_dict)
 
-# initialize the SQLalchemy engine with the provided credentials
+# initialize the SQLalchemy engine
 RDS_engine = dbc.init_db_engine(cred_dict)
 
 # import the DataExtractor class
-# it has methods to read and extract data from various sources
 from data_extraction import DataExtractor as de
+
 # get the list of tables in the RDS database
 tables_list = de.list_db_tables(RDS_engine)
-# prints the list of tables
-print(tables_list)
 
-# reads the data from the RDS table of users
+# print the list of tables
+print(tables_list)
+"""
+User data
+"""
+# read the data from the RDS table of users and stores in the users_data variable
 users_data = de.read_RDS_table(tables_list[1], RDS_engine)
 
 # import the data cleaning Class
 from data_cleaning import DataCleaning as dc
 
-# the method clean_user_data cleans the data by means of pandas
+# clean the user data via the relative method
 user_data_clean = dc.clean_user_data(users_data)
  
-#now upload the user data to the server
+# upload the user data to the SQL server
 dbc.upload_to_db(user_data_clean, 'dim_users')
-#print(df)
 
-print(user_data_clean)
-
-# extract all data from the following like
+"""
+card data
+"""
+# extract the card data
 pdf_link = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'
 card_data = de.retrieve_pdf_data(pdf_link)
-# print the data as they were extracted
-print(card_data)
+
 # clean the data through the relative method
 card_data_clean = dc.clean_card_data(card_data)
-# print the clean data
-print(card_data_clean)
 
+# upload the card data to the SQL server
 dbc.upload_to_db(card_data_clean, 'dim_card_details')
 
+"""
+Stores data
+"""
+# key and url to access the number of stores 
 key = {'x-api-key': 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
 url = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores'
 
+# retrieve the number of stores
 number_of_stores = de.list_number_of_stores(url, key)
+
+# print the number of stores
 print("the number of stores to be extracted is: ", number_of_stores)
 
-
+# url for the stores information
 base_url = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/'
 
+# download the information for each store
 stores_df = de.retrieve_stores_data(base_url, number_of_stores, key)
 
-print(stores_df)
-#stores_df.to_csv('stores_data.csv')
+# clean the stores data
 stores_data_clean = dc.clean_store_data(stores_df)
 
-
+# upload the stores data to the SQL database
 dbc.upload_to_db(stores_data_clean, 'dim_store_details')
 
-
+"""
+Product data data
+"""
 # log into AWS CLI with (i) Access Key ID and (ii) Secret Access Key
-#aws configure
-
 import subprocess
 
 # Pause script execution
@@ -77,17 +96,20 @@ input("Press Enter to pause the script and log in to AWC CLI")
 # Log into AWS CLI
 subprocess.call(["aws", "configure"])
 
+# extract the products data from the s3 server
 products_df = de.extract_from_s3('s3://data-handling-public/', filename = 'products.csv')
-#print(df)
 
+# clean the data
 products_df_clean = dc.clean_product_data(products_df)
 products_df_clean_converted_units = dc.clean_product_weights(products_df_clean)
 
-
+# upload to the SQL server
 dbc.upload_to_db(products_df_clean_converted_units, 'dim_products')
-#print(tables_list)
+
+"""
+orders data
+"""
 orders_data = de.read_RDS_table(tables_list[2], RDS_engine)
-#orders_data.to_csv('orders_data.csv')
 orders_data_clean = dc.clean_orders_data(orders_data)
 
 dbc.upload_to_db(orders_data_clean, 'orders_table')
