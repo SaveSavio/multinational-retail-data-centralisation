7c4b767c910d5a0aa66d0aba615642bd04a69a62
Saverio Tufano <saverio.tufano@gmail.com>
2023-06-06 21:49:40 +0100

    Milestone 4 Task 7 completed

--- Milestone_4_EDA.sql
+++ Milestone_4_EDA.sql
@@ -213,3 +213,26 @@ The query should return the following information:
     ORDER BY
         total_sales DESC
     LIMIT 10;
+
+/* The operations team would like to know the overall staff numbers in each location around the world.
+Perform a query to determine the staff numbers in each of the countries the company sells in.
+The query should return the values:
+
++---------------------+--------------+
+| total_staff_numbers | country_code |
++---------------------+--------------+
+|               13307 | GB           |
+|                6123 | DE           |
+|                1384 | US           |
++---------------------+--------------+
+ */
+
+SELECT
+    SUM(staff_numbers) as total_staff_numbers,
+    country_code
+FROM
+    dim_store_details
+GROUP BY
+    country_code
+ORDER BY
+    total_staff_numbers DESC;
