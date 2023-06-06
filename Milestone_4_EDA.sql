be75dbecb23640a74d8bf0e84949a21edef52b95
Saverio Tufano <saverio.tufano@gmail.com>
2023-06-06 21:42:03 +0100

    Milestone 4 Task 6 completed

--- Milestone_4_EDA.sql
+++ Milestone_4_EDA.sql
@@ -146,7 +146,8 @@ WITH table_1 AS
         dim_products
     ON
 	    orders_table.product_code = dim_products.product_code
-    ), table_2 AS
+    ),
+    table_2 AS
     (
     SELECT
         SUM(dim_products.product_price * orders_table.product_quantity) AS total_sales,
@@ -171,3 +172,44 @@ WITH table_1 AS
         ROUND(CAST(total_sales AS numeric),2),
         ROUND(CAST(total_sales/grand_total * 100 AS NUMERIC),2) as "percentage_total(%)"
     FROM table_1, table_2;
+
+
+/* The company stakeholders want assurances that the company has been doing well recently.
+Find which months in which years have had the most sales historically.
+The query should return the following information:
+
++-------------+------+-------+
+| total_sales | year | month |
++-------------+------+-------+
+|    27936.77 | 1994 |     3 |
+|    27356.14 | 2019 |     1 |
+|    27091.67 | 2009 |     8 |
+|    26679.98 | 1997 |    11 |
+|    26310.97 | 2018 |    12 |
+|    26277.72 | 2019 |     8 |
+|    26236.67 | 2017 |     9 |
+|    25798.12 | 2010 |     5 |
+|    25648.29 | 1996 |     8 |
+|    25614.54 | 2000 |     1 |
++-------------+------+-------+
+ */
+
+    SELECT
+        ROUND( CAST (SUM (dim_products.product_price * orders_table.product_quantity) AS NUMERIC), 2) AS total_sales,
+        EXTRACT(YEAR FROM time_stamp) AS year,
+        EXTRACT(MONTH FROM time_stamp) AS month
+    FROM
+        orders_table
+    LEFT JOIN
+        dim_products
+    ON
+	    orders_table.product_code = dim_products.product_code
+    LEFT JOIN
+        dim_date_times
+    ON
+        orders_table.date_uuid = dim_date_times.date_uuid
+    GROUP BY
+        year, month
+    ORDER BY
+        total_sales DESC
+    LIMIT 10;
--- test_stuff.sql
+++ test_stuff.sql
@@ -1,35 +1,48 @@
-WITH table_1 AS
-    (
+/* The company stakeholders want assurances that the company has been doing well recently.
+Find which months in which years have had the most sales historically.
+The query should return the following information:
+
++-------------+------+-------+
+| total_sales | year | month |
++-------------+------+-------+
+|    27936.77 | 1994 |     3 |
+|    27356.14 | 2019 |     1 |
+|    27091.67 | 2009 |     8 |
+|    26679.98 | 1997 |    11 |
+|    26310.97 | 2018 |    12 |
+|    26277.72 | 2019 |     8 |
+|    26236.67 | 2017 |     9 |
+|    25798.12 | 2010 |     5 |
+|    25648.29 | 1996 |     8 |
+|    25614.54 | 2000 |     1 |
++-------------+------+-------+
+ */
+
     SELECT
-        SUM(dim_products.product_price * orders_table.product_quantity) AS grand_total
+        ROUND(CAST SUM(dim_products.product_price * orders_table.product_quantity AS NUMERIC) AS total_sales,
+        EXTRACT(YEAR FROM time_stamp) AS year,
+        EXTRACT(MONTH FROM time_stamp) AS month
     FROM
         orders_table
     LEFT JOIN
         dim_products
     ON
 	    orders_table.product_code = dim_products.product_code
-    ), table_2 AS
-    (
-    SELECT
-        SUM(dim_products.product_price * orders_table.product_quantity) AS total_sales,
-        store_type
-    FROM
-        orders_table
     LEFT JOIN
-        dim_products
+        dim_date_times
     ON
-        orders_table.product_code = dim_products.product_code
-    LEFT JOIN
-        dim_store_details
-    ON
-        dim_store_details.store_code = orders_table.store_code
+        orders_table.date_uuid = dim_date_times.date_uuid
     GROUP BY
-        store_type
+        year, month
     ORDER BY
         total_sales DESC
-    )
-    SELECT
-        store_type,
-        ROUND(CAST(total_sales AS numeric),2),
-        ROUND(CAST(total_sales/grand_total * 100 AS NUMERIC),2) as "percentage_total(%)"
-    FROM table_1, table_2;
+    LIMIT 10;
+
+
+
+-- SELECT EXTRACT(MONTH FROM date_column) AS month,
+--        EXTRACT(YEAR FROM date_column) AS year,
+--        SUM(sales_amount) AS total_sales
+-- FROM your_table
+-- GROUP BY EXTRACT(MONTH FROM date_column), EXTRACT(YEAR FROM date_column)
+-- ORDER BY year, month;
